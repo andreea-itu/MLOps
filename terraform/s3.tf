32d53d9363653602f642cdf52fc41da5c9d521ea
Andreea <andreea.itu.ext@siemens-energy.com>
2026-05-18 16:34:29 +0200

    Remove infra

--- terraform/s3.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_s3_bucket" "this" {
-#   bucket = "mlopspostgradestate"
-#   region = 
-#   tags = {
-#     Name        = "state"
-#     Environment = "dev"
-#     Project     = "mlops"
-#   }
-# }
