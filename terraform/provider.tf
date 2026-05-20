b8f198aea2ee330a6d34cf09bb37b0527165a528
Andreea <andreea.itu.ext@siemens-energy.com>
2026-05-20 14:35:28 +0200

    Update envs

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,13 @@
 terraform {
+  required_version = ">= 1.10.0"
+
   required_providers {
-    terraform = {
-      required_version = ">= 1.10.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
+
   backend "s3" {
   }
 }
