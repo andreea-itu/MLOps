7b3c21542031c6e97c85cd4c2ecb11702dc13d01
Andreea <andreea.itu.ext@siemens-energy.com>
2026-05-26 18:58:20 +0200

    Update workflows

--- terraform/apprunner_services.tf
+++ terraform/apprunner_services.tf
@@ -1,6 +1,6 @@
 module "apprunner_service" {
   for_each = { for ars in var.apprunner_services : ars.key => ars }
-  source   = "./modules/apprunner-service"
+  source   = "./modules/app-runner-service"
 
   name                 = join(var.delimiter, [each.value.key, var.environment])
   source_configuration = each.value.source_configuration
